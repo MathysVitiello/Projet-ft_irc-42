7fe2ae918e040923add4e113782398b75dbf179b
Mathys Vitiello <mvitiell@3C1.42angouleme.fr>
2024-01-04 16:25:08 +0100

    jai rearder une video

--- main.cpp
+++ main.cpp
@@ -11,8 +11,100 @@
 /* ************************************************************************** */
 
 #include <iostream>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <string>
 
+//!https://www.youtube.com/watch?v=cNdlrbZSkyQ
+ 
 int main(void){
 
-    std::cout << "yeah" <<std::endl;
+    //int socket(int domain, int type, int protocol);
+
+    //create a socket
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == -1)
+    {
+        std::cerr << "Can't create a socket !";
+        return 1;
+    }
+    //std::cout << listening <<std::endl;
+
+    //Bind socket to IP adress / port
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons(54000); //== host to network short
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr); // internet command, coverts nb to an array of int | 0.0.0.0 veut dire que cest nimporte quelle adresse
+
+    if (bind(listening, (sockaddr *) &hint, sizeof(hint)) == -1)
+    {
+        std::cerr << "Can't bind to IP/port !";
+        return 2;
+    }
+
+    //Mark the socket for listening in
+    if (listen(listening, SOMAXCONN) == -1)
+    {
+        std::cerr << "Can't listen !";
+        return 3;
+    }
+
+    //Accept a call
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+   // char host(NI_MAXHOST); //max 1025
+   // char service(NI_MAXSERV); //max 32
+
+    int clientSocket = accept(listening, (sockaddr *) &client, &clientSize);
+
+    if (clientSocket == -1)
+    {
+        std::cerr << "Problem with client connecting";
+        return 3;
+    }
+
+    close(listening);
+
+    //memset(host, 0, NI_MAXHOST);
+    //memset(service, 0, NI_MAXSERV);
+
+//!
+    /*int result = getnameinfo((sockaddr *)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0);
+    if (result)
+    {
+        std::cout << host << " connected on " << service << std::endl;
+    }
+    else 
+    {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        std::cout << host << " connected on " << nthos(client.sin_port) << std::endl;
+
+    }*/
+    //while receiving display message
+    char buf[4096];
+    while(true){
+        memset(buf, 0, 4096);
+        int bytesRecv = recv(clientSocket, buf, 4096, 0);
+        if (bytesRecv == -1)
+        {
+            std::cerr << "There was a connetion issue" << std::endl;
+            break;
+        }
+        if (bytesRecv == 0)
+        {
+            std::cerr << "The client disconnected" << std::endl;
+            break;
+        }
+
+        std::cout << "Received: " << std::string(buf, 0, bytesRecv) << std::endl;
+        send(clientSocket, buf, bytesRecv + 1, 0);
+    }
+
+    close(clientSocket);
+    return 0;
 }
---
+++ mathys_notes.cpp
@@ -0,0 +1,4 @@
+// ./ircserv <port> <password>
+
+//Un socket, INT représente un lien direct avec une machine distante et est une route d’échange de données bilatérale avec celle-ci.
+
