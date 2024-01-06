4fa48e0f40765a194771f7e75dffdedde8ea4789
Mathys Vitiello <mvitiell@3D3.42angouleme.fr>
2024-01-06 14:22:11 +0100

    class en cours

--- PassNickUser.cpp
+++ PassNickUser.cpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   PassNickUser.cpp                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mvitiell <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/04 11:44:21 by mvitiell          #+#    #+#             */
-/*   Updated: 2024/01/04 11:44:22 by mvitiell         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "library.hpp"
 
 /*
@@ -22,36 +10,35 @@ L'ordre des commandes recommandé pour l'enregistrement d'un client est le suiva
 2. Message NICK
 3. Message USER
 */
-//! les clients savent quils doivent faire ca
 
 //NICK USER
-void NICK_USER(int clientSocket){
+void NICK_USER(int clientSocket, Client newClient){
 
     char buf[4096];
     int i = 0;
     int j = 0;
 
     while(i != 1 || j != 1) {
+        
+        memset(buf, 0, 4096);
         int bytesRecv = recv(clientSocket, buf, 4096, 0);
 
         if (std::string(buf, 0, bytesRecv).find("NICK ") == 0)
         {
-             std::cout << buf + 5 << " ici" << std::endl;
-
-            std::cout << clientSocket << " le reste deviens le nickname, a faire" << std::endl;
-
-            //
-
-
-
+            buf[strlen(buf)-1] = '\0';
+            newClient.setNick(buf + 5); //! set nick
             i = 1;
         }
         if (std::string(buf, 0, bytesRecv).find("USER ") == 0)
         {
-            std::cout << clientSocket << " le reste deviens le USER, a faire" << std::endl;
+            buf[strlen(buf)-1] = '\0';
+            newClient.setName(buf + 5); //! set name            
             j = 1;
         }
     }
+
+    // afficher tout 
+	newClient.showInfo();
 }
 
 //PASS
@@ -59,6 +46,9 @@ void PASS(int clientSocket, char *argv){
 
     char buf[4096];
     int i = 0;
+    
+    Client newClient; //! le declarer ailleurs, plus tot, a chque tentative de connexion / dans le main
+
 
     while(i == 0) {
         int bytesRecv = recv(clientSocket, buf, 4096, 0);
@@ -68,11 +58,9 @@ void PASS(int clientSocket, char *argv){
             std::string(buf, 0, bytesRecv).find(argv) == 5)
         {
             std::cout << clientSocket << " is now connected" << std::endl;
-
-            //!idclient = clienSocket
-
+            newClient.setId(clientSocket); //! set id
             i = 1;
         }
     }
-    NICK_USER(clientSocket);
+    NICK_USER(clientSocket, newClient);
 }
