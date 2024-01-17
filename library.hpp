304c2ed52459fa7277704df6efec51be064cca19
Alex Lamizana <alamizan@3C2.42angouleme.fr>
2024-01-17 12:46:20 +0100

    refrech branch alex

--- library.hpp
+++ library.hpp
@@ -1,35 +1,37 @@
 #ifndef LIBRARY_HPP
-# define LIBRARY_HPP
+#define LIBRARY_HPP
 
 // -------------------- DEFINE COLOR ---------------------- //
-# define CRED		"\033[6;91m"
-# define BRED		"\033[1;91m"
-# define BPURPLE 	"\001\e[1;95m\002"
-# define NC			"\001\e[0m\002"
+#define CRED		"\033[6;91m"
+#define BYELLOW		"\001\e[1;33m\002"
+#define BGREEN		"\001\e[1;32m\002"
+#define BRED		"\033[1;91m"
+#define BPURPLE 	"\001\e[1;95m\002"
+#define NC			"\001\e[0m\002"
 
 // --------------------  LIBRARY --------------------- //
-# include <unistd.h>
-# include <iostream>
-# include <stdlib.h>
-# include <stdio.h>
-# include <string.h>
-# include <vector>
-# include <algorithm>    
-# include <netinet/in.h>
-# include <netdb.h>
-# include <arpa/inet.h>
-# include <sys/types.h>
-# include <sys/select.h>
-# include <sys/socket.h>
-# include <functional>   
-# include <errno.h>
+#include <unistd.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <algorithm>    
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <functional>   
+#include <errno.h>
+#include <signal.h>
 
 // --------------------- CLASS ---------------------- //
-# include "./Client/Client.hpp"
-# include "./Server/Server.hpp"
-# include "./Channel/Channel.hpp"
-# include "define.hpp"
-# include "utils.hpp"
-
+#include "./Client/Client.hpp"
+#include "./Server/Server.hpp"
+#include "./Channel/Channel.hpp"
+#include "define.hpp"
+#include "utils.hpp"
 
 #endif
