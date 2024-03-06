646f68b3285a7072b12326e34c380a59653eebe4
Mathys Vitiello <mvitiell@3C7.42angouleme.fr>
2024-03-06 16:34:05 +0100

    projet clean et traduis

--- main.cpp
+++ main.cpp
@@ -30,31 +30,30 @@ int main(int argc, char **argv)
 		socklen_t			clientLen;                 
 
 		// ------------------------------------------------------------- //
-		// [1] Gestion arguments:
+		// [1] Argument management
 		checkArgs( argc, argv );
 
 		// ------------------------------------------------------------- //
-		// [2] Creer un serveur et le configurer:
+		// [2] Create server and configuration:
 		Server server( atoi(argv[1]), argv[2] );
 
 		// ------------------------------------------------------------- //
-		// [3] integre le serveur dans une liste de "fd":
-		// fd_set: c est une structure de données qui contient un ensemble 
-		// de descripteurs d'archives:
+		// [3] Put server in "fd" list:
+		// fd_set: data struct of fd
 		fd_set	master;
 		
 
-		// FD_ZERO permet de supprimer tous les bits d une structure fd_set.
+		// FD_ZERO is able to delete all the bits from structure fd_set.
 		FD_ZERO(&master);
-		// Integre ce nouvel ensemble de donnees dans le descripteur d archive fd_set master/
+		// Integrate this new dataset into fd_set master/
 		FD_SET(server.getSocket(), &master);
 
 		// ------------------------------------------------------------- //
-		// [4] Boucle du serveur:
-
-		// Gestion des signaux:
 		signal( SIGQUIT, SIG_IGN );
 		signal( SIGINT, sigHandler );
+
+		// ------------------------------------------------------------- //
+		// [4] Server loop:
 
 		while ( 1 )
 		{
@@ -67,7 +66,7 @@ int main(int argc, char **argv)
 			/* this is done for new connections */
 			if( FD_ISSET(server.getSocket(), &copy) )   /* new client has requested connection */
 			{
-				// [5] Ajout de clients:
+				// [5] Add clients:
 				clientLen = sizeof(clientaddr);
 				if( (newSocket = accept(server.getSocket(), (struct sockaddr *)&clientaddr, 
 								&clientLen)) == -1 )
--- utils.hpp
+++ utils.hpp
@@ -4,6 +4,4 @@
 #include "library.hpp"
 
 std::string	trimSpace(std::string str);
-void	    checkArgs(int argc, char **argv);
-int         indexFirstArg( std::string str );
 #endif
